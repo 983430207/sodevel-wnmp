a660de387f60342a053fdb7ecae8cfb7ab9dfa79
1024编程实验室 <983430207@qq.com>
2018-09-19 20:33:34 +0800

    auto open 127.0.0.1

--- 启动全部.bat
+++ 启动全部.bat
@@ -1,5 +1,4 @@
 @echo off
-chcp 65001
 set base_path=%cd%
 set nginx_path=%base_path%\nginx-1.14.0
 set php_path=%base_path%\php-7.1.17
@@ -15,6 +14,7 @@ echo Starting nginx...
 RunHiddenConsole %nginx_path%\nginx.exe -c %nginx_path%\conf\nginx.conf
 echo Starting MySql...
 RunHiddenConsole %mysql_path%\bin\mysqld --defaults-file=%mysql_path%\my.ini --port=3306
-echo 启动成功，请打开浏览器输入 127.0.0.1 测试效果（本窗口可以关闭）
-rem ping -n 3 127.0.0.1 > nul
-rem exit
+echo please open http://127.0.0.1 ...
+ping -n 3 127.0.0.1 > nul
+start iexplore "http://127.0.0.1/phpmyadmin"
+exit
